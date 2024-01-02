740b21eb8be3e1aa2c02aa8f4d3659ba3de4607c
mizukiyoshino1 <mizukiyoshino32@gmail.com>
2024-01-02 12:34:15 +0900

    過去の情報へジャンプできる部品を実装

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 /**
- * １マス分のコンポーネント
+ * ボード１マス分のコンポーネント
  * 
  */
 function Square({ value, onSquareClick }) {
@@ -12,16 +12,13 @@ function Square({ value, onSquareClick }) {
 }
 
 /**
- * メインのコンポーネント
+ * ボード全体のコンポーネント
  * 
  */
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay }) {
   // クリック時処理
   function handleClick(i) {
-    if(squares[i] || calculateWinner(squares)){
+    if(calculateWinner(squares) || squares[i]){
       return;
     }
     const nextSquares = squares.slice();
@@ -30,8 +27,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   // 勝敗判定
@@ -63,29 +59,78 @@ export default function Board() {
       </div>
     </>
   )
+}
 
-  /**
-   * 勝敗判定
-   * 
-   */
-  function calculateWinner(squares){
-    const lines = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6],
-    ];
+/**
+ * 勝敗判定処理
+ * 
+ */
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
 
-    for(let i = 0;i < lines.length; i++){
-      const [a,b,c] = lines[i];
-      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-        return squares[a];
-      }
+  for(let i = 0;i < lines.length; i++){
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
     }
-    return null;
   }
+  return null;
+}
+
+/**
+ * メインのコンポーネント
+ * 
+ */
+export default function Game(){
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  //過去の情報へジャンプできるボタン作成処理
+  const moves = history.map((squares, move) => {
+    let description;
+    if(move > 0){
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  //ボートクリック時の処理
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  //過去の情報へ戻る処理
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
 }
